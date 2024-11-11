e0c331d1c9ad4705a01690b4d50b939e4eb9113e
anor23 <f@>
2024-11-11 23:20:13 +0300

    обнаружен баг в 2 задании неполностью выполненно баг исправлен задачи решены оставлен только один вариант решения

--- dict,tuple,set/dz.11.11dict.py
+++ dict,tuple,set/dz.11.11dict.py
@@ -12,42 +12,35 @@ from os.path import split
 #
 # print(tuple(sorted( numbers,reverse=True)), tuple(sorted( genres,reverse=True)))
 
-cinema_genres = ("Комедия", "Экшн", "Пеплум", "Триллер")
-# user_input =input()
-user_input = "Боевик"
-user_replace = "Экшн"
-new_tuple = ""
+# cinema_genres = ("Комедия", "Экшн", "Пеплум", "Триллер")
+# # user_input =input()
+# user_input = "Боевик"
+# user_replace = "Экшн"
+# new_tuple = ""
 # for genge in cinema_genres:
 #     if genge == user_replace:
 #         new_tuple+=" "+user_input
+#         new_tuple+=" Аниме"
 #     else:
 #         new_tuple+=" "+genge
-
+#
+#
 # print(tuple(new_tuple.split()))
-# 2 вариант решения
-# new_tuple = " ".join(cinema_genres)
-# new_tuple = new_tuple.replace(user_replace, user_input).split()
-# print(tuple(new_tuple))
+# print("Обновленые жанры кино:",new_tuple)
 
-# 3 вариант решения
-#
-# new_tuple = tuple(user_input if genre==user_replace else genre for genre in cinema_genres)
-# print(new_tuple)
-#
-#
 
 
 # 3 task
 
 
-my_things = {"axe", "boots", 'apple', "book", "lighter" "boat"}
-he_things = {"axe","monkey","apple","book","dog","rope"}
-
-my_and_he_things = my_things.union(he_things)
-onlY_my_tings = my_things.difference(he_things)
-onlY_he_tings = he_things.difference(my_things)
-my_and_he_only = my_things.intersection(he_things)
-print(my_and_he_things)
-print(onlY_my_tings)
-print(onlY_he_tings)
-print(my_and_he_only)
+# my_things = {"axe", "boots", 'apple', "book", "lighter" "boat"}
+# he_things = {"axe","monkey","apple","book","dog","rope"}
+#
+# my_and_he_things = my_things.union(he_things)
+# onlY_my_tings = my_things.difference(he_things)
+# onlY_he_tings = he_things.difference(my_things)
+# my_and_he_only = my_things.intersection(he_things)
+# print(my_and_he_things)
+# print(onlY_my_tings)
+# print(onlY_he_tings)
+# print(my_and_he_only)
