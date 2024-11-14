510b8c1d32732ae82d452aae0f138d88716706ca
anor23 <f@>
2024-11-14 12:20:17 +0300

    импорт базы данных в основной  файл рефакторинг для улучшения читаемости

--- dict,tuple,set/dictionarydz14.11.py
+++ dict,tuple,set/dictionarydz14.11.py
@@ -1,17 +1,20 @@
+from dictionary_data_base import *
+
+# я знаю что таким импортом лучше не пользоваться но мне показалось что в рамках этого задания это допустимо
+# мне не захотелось менять имена всех переменных и  я захотел вот так сделать
 
 
-user_input= input("выберете уровень сложности  легкий  средний сложный :")
+user_input = input("выберете уровень сложности  легкий  средний сложный :")
 
-for word,translate in words[user_input].items():
+for word, translate in words[user_input].items():
     print(f"и так слово {word} перевод  {len(translate)} букв    начинается с {translate[0]}")
-    user_answer=input("введите ваш ответ:").lower().strip()
+    user_answer = input("введите ваш ответ:").lower().strip()
     if user_answer == translate:
         print(f"верно")
-        score+=1
+        score += 1
     else:
         print(f"неверно переводится как {translate}")
-    answers[word]=user_answer == translate
+    answers[word] = user_answer == translate
 
-
-[print("слово",i,"отвечено верно") if k else print("слово",i,"отвечено неверно") for i,k in answers.items()]
+[print("слово", i, "отвечено верно") if k else print("слово", i, "отвечено неверно") for i, k in answers.items()]
 print(f"ваш ранг\n{levels[score]}")
