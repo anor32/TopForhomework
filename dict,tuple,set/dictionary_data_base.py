8dc54e4effdec75bdf3ec81d013958428cc91131
anor23 <f@>
2024-11-14 12:13:09 +0300

    перенос словарей с одного файла на другой для улучшения читаемости

---
+++ dict,tuple,set/dictionary_data_base.py
@@ -0,0 +1,39 @@
+
+words_easy = {
+    "family": "семья",
+    "hand": "рука",
+    "people": "люди",
+    "evening": "вечер",
+    "minute": "минута",
+}
+words_medium = {
+    "believe": "верить",
+    "feel": "чувствовать",
+    "make": "делать",
+    "open": "открывать",
+    "think": "думать",
+}
+words_hard = {
+    "rural": "деревенский",
+    "fortune": "удача",
+    "exercise": "упражнение",
+    "suggest": "предлагать",
+    "except": "кроме",
+}
+
+levels = {
+    0: "Нулевой",
+    1: "Так себе",
+    2: "Можно лучше",
+    3: "Норм",
+    4: "Хорошо",
+    5: "Отлично"
+}
+
+
+words={"легкий":words_easy,
+       "средний":words_medium,
+       "сложный":words_hard,
+}
+answers={}
+score=0
