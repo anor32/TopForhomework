c5e544742789520e9ec29abc9f915d230fcdf6cc
anor23 <f@>
2024-11-22 21:22:14 +0300

    выполнено задание оптимизация кода перенос некоторых функции во второй файл добавлены коментарии

--- dict,tuple,set/EnglishMain.py
+++ dict,tuple,set/EnglishMain.py
@@ -1,33 +1,27 @@
-import json
-from englishUtils import get_user_level, base_program, get_result
-python_data={}
-try:
-    with open("user_inputs.json", "r", encoding="utf-8") as f:
-       json_data= f.read()
-       python_data =json.loads(json_data)
-except (FileNotFoundError):
-    print("файл не найден")
+from englishUtils import get_user_level
+from englishUtils import base_program
+from englishUtils import get_result
+from englishUtils import read_file_with_user_input
+from englishUtils import write_stats
 
-    if python_data=={}:
-        user_name = input("введите ваше имя")
-        dificulty = input("выберете уровень сложности  легкий  средний сложный :").strip().lower()
-    else:
-        dificulty = python_data["уровень сложности"]
-        user_name = python_data["имя пользователя"]
+python_data = read_file_with_user_input("user_inputs.json")
+if python_data == {}:
+    user_name = input("введите ваше имя")
+    dificulty = input("выберете уровень сложности  легкий  средний сложный :").strip().lower()
+else:
+    user_name, dificulty = python_data.values()
 
-level = get_user_level(dificulty)
-answers = base_program(level)
+user_level = get_user_level(dificulty)
+answers = base_program(user_level)
 result = get_result(answers)
 print("ваш Ранг", result)
 
+write_stats(user_name,user_level,result)
 
-
-
-with open(f"statistics{user_name}.json","w",encoding="utf-8") as file:
-    stats = {"Name": user_name,
-             "words_translated_user": level,
-             "resultUser": result,
-             }
-    user_stats = json.dumps(stats, ensure_ascii=False, indent=4)
-    file.write(user_stats)
-
+# возможно функции для прочитывания файла
+# не надо было  заводить  в отдельный файл ну  я решил так
+# я стараюсь в основном файле как можно меньше строк делать
+# я сделал файл user_inputs для имитации файлов которые будут приходить от внешних источников
+# в задании ничего не скзано проних как они должны приходить в сколько слов какие ключи и тд
+# поэтому я сделал на свое усмотрение
+# я так полагаю главное тут было прочитать файл и взять из него данные  ну и конвертировать
