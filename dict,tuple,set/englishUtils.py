ec80a6115d3a294d3759579cd775650daff7b0ed
anor23 <f@>
2024-11-22 21:22:57 +0300

    задание выполнено добавлены новые функции для чтения файла  и сбора статистики

--- dict,tuple,set/englishUtils.py
+++ dict,tuple,set/englishUtils.py
@@ -1,3 +1,6 @@
+import json
+from json import JSONDecodeError
+
 __levels = {"легкий":
                 {"family": "семья",
                  "hand": "рука",
@@ -47,3 +50,33 @@ def get_result(answers: dict):
     result = len([key for key, value in answers.items() if value])
 
     return __ranks[result]
+
+# нормально ли вообще так делать чтобы исключения функцию засовывать
+# в целом для меня выглядит приемлимо
+# обычно я исключения не делаю
+# но тут работа с файлами поэтому
+# фиг его знает какой файл придет
+def read_file_with_user_input(path_file):
+    python_data={}
+    try:
+        with open(path_file, "r", encoding="utf-8") as f:
+            json_data = f.read()
+            python_data = json.loads(json_data)
+
+    except FileNotFoundError:
+        print("файл не найден")
+
+    except JSONDecodeError:
+        print("неверный формат или файл пуст")
+
+    return python_data
+
+def write_stats(user_name,level,result):
+    with open(fr"user_statisks\statistics{user_name}.json", "w", encoding="utf-8") as file:
+        stats = {"Name": user_name,
+                 "words_translated_user": level,
+                 "resultUser": result,
+                 }
+        user_stats = json.dumps(stats, ensure_ascii=False, indent=4)
+        file.write(user_stats)
+    print("данные записаны")
